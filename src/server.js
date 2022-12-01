1e1b180b698e6d44d2b7602bb6bf0db593f52abf
chienlx1005 <chienlx1005@gmail.com>
2022-12-01 13:57:12 +0700

    cors

--- src/server.js
+++ src/server.js
@@ -15,12 +15,8 @@ const port = process.env.HOST || 8080;
 connectDB();
 
 // install
-const corsOptions = {
-  origin: "http://localhost:3000",
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-};
-app.use(cors(corsOptions));
+
+app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 app.use(bp.urlencoded({ extended: true }));
